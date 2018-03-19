652205e4d5198c2af891e04cb666365a58461737
Lucas Augusto <lucas.tansini@hotmail.com>
2018-03-19 00:45:27 -0300

    Adição de melhor feedback para identificadores na HashTable

--- etapa1/hash.c
+++ etapa1/hash.c
@@ -31,6 +31,7 @@ HASH* hashInsert(int type, char *text){
 
   newNode = (HASH*) calloc(1,sizeof(HASH));
   address = hashAddress(text);
+  newNode->type = type;
   newNode->yytext = calloc(strlen(yytext)+1,sizeof(char));
   strcpy(newNode->yytext,text);
   newNode->next = table[address];
@@ -47,7 +48,26 @@ void hashPrint(){
   int i = 0;
   for(i = 0 ; i<HASH_SIZE; i++){
     for(print_node = table[i]; print_node ; print_node=print_node->next){
-      printf("Hashtable[%d] = %s\n",i,print_node->yytext);
+      switch(print_node->type){
+          case TK_IDENTIFIER:
+              printf("HashTable[%d] contains %s and is TK_IDENTIFIER\n",i,print_node->yytext);
+              break;
+          case LIT_CHAR:
+            printf("HashTable[%d] contains %s and is LIT_CHAR\n",i,print_node->yytext);
+            break;
+          case LIT_STRING:
+            printf("HashTable[%d] contains %s and is LIT_STRING\n",i,print_node->yytext);
+            break;
+          case LIT_REAL:
+            printf("HashTable[%d] contains %s and is LIT_REAL\n",i,print_node->yytext);
+            break;
+          case LIT_INTEGER:
+            printf("HashTable[%d] contains %s and is LIT_INTEGER\n",i,print_node->yytext);
+            break;
+          default:
+            printf("Not identified!");
+      }
+
     }
   }
 
